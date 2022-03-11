0b9b84bf03e187cd960cca400bbe7b29972b4567
gabrieluntura <38741621+gabrieluntura@users.noreply.github.com>
2022-03-11 23:49:20 +0100

    Create wpgridbuilder_map-markers-based-taxonomy.php

---
+++ wpgridbuilder_map-markers-based-taxonomy.php
@@ -0,0 +1,48 @@
+<?php
+add_filter(
+	'wp_grid_builder_map/geojson',
+	function( $json, $facet ) {
+ 
+		$json['features'] = array_map(
+			function( $feature ) {
+ 
+				$icon_url  = '';
+				$post_id   = $feature['properties']['id'];
+				$terms = get_the_terms( $post_id, 'YOURTAXONOMYHERE' );
+                
+                if ( !empty( $terms ) ){
+                // get the first term
+                $term = array_shift( $terms );
+
+                // image id is stored as term meta
+                $image_id = get_term_meta( $term->term_id, 'image', true );
+                
+                // image data stored in array, second argument is which image size to retrieve
+                $image_data = wp_get_attachment_image_src( $image_id, 'full' );
+                
+                // icon url is the first item in the array (aka 0)
+                $icon_url = $image_data[0];
+
+                }
+ 
+ 
+				if ( ! empty( $icon_url ) ) {
+					$feature['properties']['icon']['url'] = $icon_url;
+				}
+				else {
+				    
+				}
+ 
+				return $feature;
+ 
+			},
+			$json['features']
+		);
+ 
+		return $json;
+ 
+	},
+	10,
+	2
+);
+Copied
